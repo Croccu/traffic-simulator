6008e00f03f417d4f5019a92af2152e951867d0d
Marcus Puust <marcusp@tlu.ee>
2025-06-19 23:33:13 +0300

    gn youtube link

--- Assets/Scripts/UI/OpenYoutube.cs
+++ Assets/Scripts/UI/OpenYoutube.cs
@@ -4,7 +4,7 @@ public class OpenYoutube : MonoBehaviour
 {
     public void OpenLink()
     {
-        Application.OpenURL("https://www.youtube.com/watch?v=nIrgVJTXj0k");
+        Application.OpenURL("https://www.youtube.com/watch?v=KpjJFDQ8SgU");
     }
 }
 
